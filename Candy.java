d6d78bcb11af8aecf24a7e916e89235267a479a3
staubamc <89548898+staubamc@users.noreply.github.com>
2021-10-26 12:45:03 +0000

    Initial commit

---
+++ Candy.java
@@ -0,0 +1,30 @@
+/** 
+* This class  
+* @author Maria Staubach 
+* @version 1
+* @param 
+*/
+
+
+class Candy {
+  String name; 
+
+Candy(){
+  name = "";
+}
+
+Candy(String aName){
+  name = aName;
+}
+
+void setName(String theName){
+  name = theName;
+}
+
+String getName(){
+  return name;
+}
+
+
+}
+
